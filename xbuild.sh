f2a391204cd8f4859e8ae85d8c827587a9ba8615
weikangqi <3271045267@qq.com>
2024-02-26 17:16:16 +0800

    change: clean

--- xbuild.sh
+++ xbuild.sh
@@ -1,5 +1,5 @@
 cd /home/klay/MNN/build
-rm -rf *
+make clean
 cmake -DMNN_OPENCL=ON -DMNN_BUILD_DEMO=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug  -DMNN_SEP_BUILD=OFF ..
 make -j${nproc}
 rm /home/klay/MNN/out.png
